7eb6d37c2bee51ba42b98310c1d82d2f7d832dba
Raphael <raphaelaraujotoc@outlook.com>
2023-10-02 12:25:06 -0300

    Instalando o app 'usuarios'

--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -38,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # apps:
+    'usuarios',
 ]
 
 MIDDLEWARE = [
