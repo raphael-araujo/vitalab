a29e6bf8e80cc63ab24a72e370fb0254bb85e41b
Raphael <raphaelaraujotoc@outlook.com>
2023-10-03 21:44:43 -0300

    Adicionando models para o app exames

--- exames/admin.py
+++ exames/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import PedidoExame, SolicitacaoExame, TipoExame
+
 # Register your models here.
+
+
+admin.site.register(PedidoExame)
+admin.site.register(SolicitacaoExame)
+admin.site.register(TipoExame)
--- exames/models.py
+++ exames/models.py
@@ -1,2 +1,46 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
+class TipoExame(models.Model):
+    TIPO_CHOICES = (
+        ('I', 'Exame de imagem'),
+        ('S', 'Exame de sangue')
+    )
+
+    nome = models.CharField(max_length=50)
+    tipo = models.CharField(max_length=2, choices=TIPO_CHOICES)
+    preco = models.DecimalField(max_digits=7, decimal_places=2)
+    disponivel = models.BooleanField(default=False)
+    horario_inicial = models.TimeField()
+    horario_final = models.TimeField()
+
+    def __str__(self) -> str:
+        return self.nome
+
+
+class SolicitacaoExame(models.Model):
+    CHOICE_STATUS = (
+        ('E', 'Em análise'),
+        ('F', 'Finalizado')
+    )
+
+    usuario = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    exame = models.ForeignKey(TipoExame, on_delete=models.DO_NOTHING)
+    status = models.CharField(max_length=2, choices=CHOICE_STATUS)
+    resultado = models.FileField(upload_to='resultados', null=True, blank=True)
+    requer_senha = models.BooleanField(default=False)
+    senha = models.CharField(max_length=6, null=True, blank=True)
+
+    def __str__(self) -> str:
+        return f'{self.usuario} | {self.exame.nome}'
+
+
+class PedidoExame(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    exames = models.ManyToManyField(SolicitacaoExame)
+    agendado = models.BooleanField(default=True)
+    data = models.DateTimeField()
+
+    def __str__(self) -> str:
+        return f'{self.usuario} | {self.data}'
